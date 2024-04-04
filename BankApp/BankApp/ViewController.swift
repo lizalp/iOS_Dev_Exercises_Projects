2a35ef923101b663ac8476013008f91691f2a661
Liz Alpizar <liz.vaalpizar@gmail.com>
2024-04-04 09:39:53 -0600

    First attemp login view

---
+++ BankApp/BankApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp
+//
+//  Created by Liz Alpizar on 25/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
